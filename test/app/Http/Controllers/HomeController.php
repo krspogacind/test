0abb576add69ccf2874b7252d293bff1661fb77d
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-08 17:50:35 +0100

    laravel middleware

--- test/app/Http/Controllers/HomeController.php
+++ test/app/Http/Controllers/HomeController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('age')->only('index');
+    }
     /**
      * Display a listing of the resource.
      *
