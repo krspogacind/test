a9c1a533bbe54744357897b08d6ef943613c4847
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-09 18:46:47 +0100

    add users seeder

--- test/database/factories/UserFactory.php
+++ test/database/factories/UserFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
@@ -23,7 +25,9 @@ class UserFactory extends Factory
     {
         return [
             'email' => $this->faker->unique()->safeEmail,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
             'first_name' => $this->faker->firstName,
             'last_Name' => $this->faker->lastName,
             'company' => $this->faker->company,
