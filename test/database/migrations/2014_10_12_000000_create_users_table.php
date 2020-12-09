513396e562f7dd384cac7c96aafe99b8369f3c6b
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-09 15:43:37 +0100

    laravel seeding

--- test/database/migrations/2014_10_12_000000_create_users_table.php
+++ test/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique(); 
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable(); 
             $table->string('password');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('company')->nullable();
             $table->string('country')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
