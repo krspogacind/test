0cec4d548ef40d96f71318819ec090af2c99c55c
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-10 13:46:02 +0100

    laravel authentification

---
+++ test/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        $countries = DB::table('countries')->get();
+        Log::info($countries);
+        return view('auth.register', ['countries' => $countries]);
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'company' => 'string|max:255',
+            'country' => 'exists:countries,id'
+        ]);
+
+        Auth::login($user = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'company' => $request->company,
+            'country' => $request->country,
+        ]));
+
+        event(new Registered($user));
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
--- test/routes/web.php
+++ test/routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Route::resource('home', 'HomeController');
 
 Route::resource('photos', 'PhotoController');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
